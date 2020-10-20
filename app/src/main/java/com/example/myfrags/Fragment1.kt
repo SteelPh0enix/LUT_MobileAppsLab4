edff718daebea7b1ec07b502a7a66b58b3dc578b
SteelPh0enix <steelphoenix2137@gmail.com>
2020-10-20 10:33:57 +0200

    finished:

--- app/src/main/java/com/example/myfrags/Fragment1.kt
+++ app/src/main/java/com/example/myfrags/Fragment1.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +35,40 @@ class Fragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        val view = inflater.inflate(R.layout.fragment_1, container, false)
+
+        // val = cannot change reference, var = normal variable, const = compile-time constant
+        // mógłbym też zrobić tak (żeby określić typ zmiennej, ale tutaj mam od tego argument szablonu)
+        // val buttonShuffle: Button = view.findViewById etc etc
+        val buttonShuffle = view.findViewById<Button>(R.id.button_shuffle)
+        val buttonClockwise = view.findViewById<Button>(R.id.button_clockwise)
+        val buttonHide = view.findViewById<Button>(R.id.button_hide)
+        val buttonRestore = view.findViewById<Button>(R.id.button_restore)
+
+        // znacznie krótsze niż Javowe - kotlin do interfejsów jedno-metodowych pozwala przekazać
+        // lambdę, a jeśli nie ma argumentów to pozwala skrócić ją do takiej konstrukcji
+        // Resztę magii robi kompilator.
+        buttonShuffle.setOnClickListener {
+            // ? to null-check
+            // jeśli callback jest null, zwróci null
+            // jeśli nie, wykona funkcję
+            // bardzo przydatne + można to chainować
+            callback?.onButtonClickShuffle()
+        }
+
+        buttonClockwise.setOnClickListener {
+            callback?.onButtonClickClockwise()
+        }
+
+        buttonHide.setOnClickListener {
+            callback?.onButtonClickHide()
+        }
+
+        buttonRestore.setOnClickListener {
+            callback?.onButtonClickRestore()
+        }
+
+        return view
     }
 
     companion object {
@@ -55,5 +89,19 @@ class Fragment1 : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    // domyślnie jest public, private trzeba określić
+    interface OnButtonClickListener {
+        fun onButtonClickShuffle()
+        fun onButtonClickClockwise()
+        fun onButtonClickHide()
+        fun onButtonClickRestore()
+    }
+
+    private var callback: OnButtonClickListener? = null
+
+    fun setOnButtonClickListener(callback: OnButtonClickListener) {
+        this.callback = callback
     }
 }
--- app/src/main/java/com/example/myfrags/MainActivity.kt
+++ app/src/main/java/com/example/myfrags/MainActivity.kt
@@ -1,26 +1,118 @@
 package com.example.myfrags
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(), Fragment1.OnButtonClickListener {
+
+    // Array<T> zamiast T[], używa się bardzo podobnie
+    private var frames: IntArray? = null
+    private var hidden: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment1 = Fragment1()
-        val fragment2 = Fragment2()
-        val fragment3 = Fragment3()
-        val fragment4 = Fragment4()
+        if (savedInstanceState == null) {
+            frames = intArrayOf(R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4)
+            hidden = false
+
+            val fragments = arrayOf(Fragment1(), Fragment2(), Fragment3(), Fragment4())
+            val transaction = supportFragmentManager.beginTransaction()
+
+            for (i in 0..3) {
+                // !! powoduje że instrukcja zwróci wyjątek jeśli obiekt do którego się odnosimy jest null
+                transaction.add(frames!![i], fragments[i])
+            }
+
+            transaction.addToBackStack(null)
+            transaction.commit()
+
+        } else {
+            frames = savedInstanceState.getIntArray("FRAMES")
+            hidden = savedInstanceState.getBoolean("HIDDEN")
+        }
+    }
+
+    private fun createNewFragments() {
+        val newFragments = arrayOf(Fragment1(), Fragment2(), Fragment3(), Fragment4())
 
         val transaction = supportFragmentManager.beginTransaction()
-
-        transaction.add(R.id.frame1, fragment1)
-        transaction.add(R.id.frame2, fragment2)
-        transaction.add(R.id.frame3, fragment3)
-        transaction.add(R.id.frame4, fragment4)
+        for (i in 0..3) {
+            transaction.replace(frames!![i], newFragments[i])
+            if (hidden && (newFragments[i] !is Fragment1)) transaction.hide(newFragments[i])
+        }
 
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putIntArray("FRAMES", frames)
+        outState.putBoolean("HIDDEN", hidden)
+    }
+
+    override fun onButtonClickRestore() {
+        if (!hidden) return
+
+        val transaction = supportFragmentManager.beginTransaction()
+        for (fragment in supportFragmentManager.fragments) {
+            if (fragment is Fragment1) continue
+            transaction.show(fragment)
+        }
+
+        transaction.addToBackStack(null)
+        transaction.commit()
+        hidden = false
+    }
+
+    override fun onButtonClickClockwise() {
+        // BUG: po shufflowaniu nie ma pewności że fragmenty będą
+        // przesuwać się clockwise
+        val temp = frames!![0]
+        frames!![0] = frames!![1]
+        frames!![1] = frames!![2]
+        frames!![2] = frames!![3]
+        frames!![3] = temp
+
+        createNewFragments()
+    }
+
+    override fun onButtonClickHide() {
+        if (hidden) return
+
+        val transaction = supportFragmentManager.beginTransaction()
+        for (fragment in supportFragmentManager.fragments) {
+            if (fragment is Fragment1) continue
+
+            transaction.hide(fragment)
+        }
+
+        transaction.addToBackStack(null)
+        transaction.commit()
+        hidden = true
+    }
+
+    override fun onButtonClickShuffle() {
+        // tutaj powinien być zrobiony check przed tworzeniem listy, ale jestem leniwy
+        // jak walnie nullexception to walnie nullexception, na co drążyć temat ( ͡° ͜ʖ ͡°)
+        val list = frames!!.slice(0..3).shuffled()
+        for (i in 0..3) {
+            frames!![i] = list[i]
+        }
+
+        createNewFragments()
+    }
+
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+
+        if (fragment is Fragment1) {
+            fragment.setOnButtonClickListener(this)
+        }
     }
 }
